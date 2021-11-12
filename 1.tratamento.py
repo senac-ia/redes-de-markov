bf49dee0a89774914242a5551cc23dfb5a2a4565
Celso Crivelaro <celsocrivelaro@gmail.com>
2021-11-12 11:03:04 -0300

    corpo de treinamento e tratamento

---
+++ 1.tratamento.py
@@ -0,0 +1,17 @@
+machado = ["casaVelha.txt", "domCasmurro.txt", "esau.txt", "iaia.txt", "memorial-de-aires.txt", "memoriasBras.txt", "quincas.txt"]
+
+stopwords = ["-", "—"]
+
+output = open("tmp/palavras.txt", "w")
+
+for livro in machado:
+  print("Tratando livro: " + livro)
+  for linha in open("treinamento/"+livro, "r"):
+    palavras = linha.lower().split()
+    for palavra in palavras:
+      palavra = palavra.strip(" .,;!?/\"\'+-)(:")
+
+      if palavra not in stopwords:
+        output.write(palavra + "\n")
+
+print("Acabou")
